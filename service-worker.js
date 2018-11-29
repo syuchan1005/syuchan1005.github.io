e4bee9d392276022fecf9299f693c96f96ba8c98
syuchan1005 <syuchan.dev@gmail.com>
2018-11-29 20:50:42 +0900

    :+1: Add details

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.af6e6f94e6493a2c1f48602527427ca7.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.4f3555ae2cce631f176a702d2b2e81cd.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.core.setCacheNameDetails({prefix: "syuchan1005.github.io"});
 
