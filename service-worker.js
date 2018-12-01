4992bc2d59502132822cf7c5e60f02f569861f25
syuchan1005 <syuchan.dev@gmail.com>
2018-12-01 18:11:28 +0900

    :+1: Add update notification

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,13 @@
-importScripts("/precache-manifest.4f3555ae2cce631f176a702d2b2e81cd.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.e01280b90f3115b3dac47af41c9ba641.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
-workbox.core.setCacheNameDetails({prefix: "syuchan1005.github.io"});
+/* global self, workbox */
+/* eslint-env serviceworker */
+/* eslint no-underscore-dangle: ['error', { 'allow': ['__precacheManifest'] } ] */
+/* eslint-disable no-restricted-globals */
+
+workbox.core.setCacheNameDetails({ prefix: 'syuchan1005.github.io' });
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(self.skipWaiting());
-});
 
