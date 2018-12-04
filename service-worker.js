4396ce002cb2575953f9fdd295444ef5c9d0055b
syuchan1005 <syuchan.dev@gmail.com>
2018-12-04 22:02:27 +0900

    :pencil2: Add hatenablog link

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.0f8f843d3b340bb9fcd112435934fcc6.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.8f3767920ae6afc2485676bd2f20b86d.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* global self, workbox */
 /* eslint-env serviceworker */
