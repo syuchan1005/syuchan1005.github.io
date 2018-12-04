cc0f42a3ef64faf6a645c03591861905b95fb54e
syuchan1005 <syuchan.dev@gmail.com>
2018-12-04 19:59:09 +0900

    :construction: change promise system

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.4f27a50d6d0725f34fac99c96463c068.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.0f8f843d3b340bb9fcd112435934fcc6.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* global self, workbox */
 /* eslint-env serviceworker */
