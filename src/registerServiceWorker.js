b454bd94ade6dbcd04d1ab5c07a4f12263fe8860
syuchan1005 <syuchan.dev@gmail.com>
2018-12-06 13:32:15 +0900

    :bug: Update service-worker

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -4,10 +4,15 @@ import { register } from 'register-service-worker';
 
 if (process.env.NODE_ENV === 'production') {
   const isUpdateAvailable = Symbol('isUpdateAvailable');
-  window.isUpdateAvailable = new Promise((resolve) => { window[isUpdateAvailable] = resolve; });
+  window.isUpdateAvailable = new Promise((resolve) => {
+    window[isUpdateAvailable] = (reg) => {
+      reg.update();
+      resolve();
+    };
+  });
   register(`${process.env.BASE_URL}service-worker.js`, {
-    updatefound() {
-      window[isUpdateAvailable]();
+    updatefound(reg) {
+      window[isUpdateAvailable](reg);
     },
   });
 }
