e4ff284f13d0fac903ade812d7098f8cdfd8a4ae
syuchan1005 <syuchan.dev@gmail.com>
2019-02-07 01:35:43 +0000

    Update README.md

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.8d70cf62de3ded20c81d061946d09b9c.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.ff913af55bd3af148d527e15c3519dcd.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* global self, workbox */
 /* eslint-env serviceworker */
