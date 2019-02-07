12960e03834169e6747518b06a78888889543b0f
syuchan1005 <syuchan.dev@gmail.com>
2019-02-07 10:20:46 +0000

    Update main.workflow

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.995a2bf59a60902900b8245a97b9d95f.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.59eee6110fd6e2d6b5810285440199f7.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* global self, workbox */
 /* eslint-env serviceworker */
