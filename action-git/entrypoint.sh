cb50fb70456c406c49d7a60966b48df226885f57
Shuichi Sato <syuchan.dev@gmail.com>
2019-04-11 22:57:39 +0900

    Add action-git

---
+++ action-git/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -l	
+
+git config --global user.email $(cat ${GITHUB_EVENT_PATH} | jq ".pusher.email")	
+git config --global user.name  $(cat ${GITHUB_EVENT_PATH} | jq ".pusher.name")	
+
+sh -c "$*"
