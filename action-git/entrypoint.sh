5bf3f14bced6c1ae798b6c9dc493b09a5a634165
Shuichi Sato <syuchan.dev@gmail.com>
2019-04-12 17:32:41 +0900

    Update entrypoint.sh

--- action-git/entrypoint.sh
+++ action-git/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l	
+#!/bin/sh
 
 git config --global user.email $(cat ${GITHUB_EVENT_PATH} | jq ".pusher.email")	
 git config --global user.name  $(cat ${GITHUB_EVENT_PATH} | jq ".pusher.name")	
