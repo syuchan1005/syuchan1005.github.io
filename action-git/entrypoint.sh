ff9572b79d9ed6605a162d67ef1ed100fc84ab06
syuchan1005 <syuchan.dev@gmail.com>
2019-04-12 17:47:55 +0900

    Update action-git

--- action-git/entrypoint.sh
+++ action-git/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git config --global user.email $(cat ${GITHUB_EVENT_PATH} | jq ".pusher.email")	
-git config --global user.name  $(cat ${GITHUB_EVENT_PATH} | jq ".pusher.name")	
+git config --global user.email $(cat ${GITHUB_EVENT_PATH} | jq ".pusher.email")
+git config --global user.name  $(cat ${GITHUB_EVENT_PATH} | jq ".pusher.name")
 
 sh -c "$*"
