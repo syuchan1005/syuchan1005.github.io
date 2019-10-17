05934a88d4ab88f97c5c9aab3e9dee74b821c752
Shuichi Sato <syuchan.dev@gmail.com>
2019-10-17 22:53:18 +0900

    Delete main.workflow

--- action-git/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-git config --global user.email $(cat ${GITHUB_EVENT_PATH} | jq ".pusher.email")
-git config --global user.name  $(cat ${GITHUB_EVENT_PATH} | jq ".pusher.name")
-
-sh -c "$*"
